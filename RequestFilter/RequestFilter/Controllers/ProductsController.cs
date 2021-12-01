ae3808c2f64b1dc1e45f3eb70d83785c4642dfe8
Türkay Ürkmez <turkay@turkayurkmez.com>
2021-12-01 15:58:48 +0300

    Black List & White List with binding data and insecure deserializatin theory

---
+++ RequestFilter/RequestFilter/Controllers/ProductsController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RequestFilter.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RequestFilter.Controllers
+{
+    public class ProductsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create([Bind(include:"Name,Price,Description")]Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                //farz edin ki eklendi...
+                return Redirect("/");
+            }
+
+            return View();
+         
+        }
+    }
+}
